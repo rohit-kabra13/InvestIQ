c233a1bb505a0ca9b794c1b2746e0014ef65135e
rohit-kabra13 <rohitkabra13@gmail.com>
2026-07-04 19:24:50 -0400

    Add portfolio dictionary and loop to print ticker prices

--- main.py
+++ main.py
@@ -5,3 +5,5 @@ stock_prices = [150, 200, 75]
 print(stock_prices[1])
 portfolio = {"AAPL": 150, "GOOG": 200, "TSLA": 75}
 print(portfolio["TSLA"])
+for ticker in portfolio:
+    print(ticker, portfolio[ticker])
