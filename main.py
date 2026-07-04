5cf5d6b797cf9a3ff2ecb27fff187ef3ad649f7d
rohit-kabra13 <rohitkabra13@gmail.com>
2026-07-04 19:09:35 -0400

    Learning about print, variable, list and dictionary

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-portfolio_value = [150, 200, 300]
-print(portfolio_value[1])
+print("Hello InvestIQ")
+portfolio_value = 65000
+print(portfolio_value)
+stock_prices = [150, 200, 75]
+print(stock_prices[1])
+portfolio = {"AAPL": 150, "GOOG": 200, "TSLA": 75}
+print(portfolio["TSLA"])
